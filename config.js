54c5289d65b6a8409f84cd72e4bc7c17875ad509
xenon <83706751+pranav008-man@users.noreply.github.com>
2026-02-01 01:02:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "Happy 1 Year Anniversary, My Wifeyyyyyy 🎉💖",
         message: "To My Baby Girlllll — 365 days with you felt like a dream. I’d choose you again, every lifetime, My Wifeyyyy.",
-        emojis: "🥂💑💖🎂🌹✨" // These will bounce around
+        emojis: "🥂🧑🏻‍❤️‍💋‍👩🏻💖🎂🌹✨" // These will bounce around
     },
 
     // Color scheme for the website
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dteqfz1du/video/upload/v1769887797/d4vd_-_Here_With_Me_Official_Music_Video_ilyvah.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
