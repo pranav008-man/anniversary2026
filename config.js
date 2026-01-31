8d661e2fc4e3f091a5517105a901e03767eac57f
xenon <83706751+pranav008-man@users.noreply.github.com>
2026-01-31 19:15:45 +0530

    Refactor third section and adjust heart explosion size
    Removed unnecessary nesting in the third section and updated heart explosion size.

--- config.js
+++ config.js
@@ -33,7 +33,6 @@ const CONFIG = {
             nextBtn: "Moreee 💕"                                        // Text for the next button
         },
         third: {
-            third: {
                 text: "My Wifeyyyy 💍✨ will you keep choosing me every day?",
                 yesBtn: "Always 😭❤️",
                 noBtn: "Come closer…"                                                 // Text for "No" button
@@ -71,7 +70,7 @@ const CONFIG = {
         floatDuration: "20s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 2.0         // Size of heart explosion effect (1.2-2.0 recommended)
+        heartExplosionSize: 1.5        // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
